41223cac23194f888b9191e9c08b2f39449fbca8
ismailkhan11 <93599419+ismailkhan11@users.noreply.github.com>
2024-09-12 11:47:41 +0530

    Add files via upload

---
+++ Python_Day14_Tuples_Assigment 1.py
@@ -0,0 +1,16 @@
+# Write a Python program to find the number of times 4 appears in the tuple. 
+# Input: tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7 ) Output: 3 
+
+# Define the tuple with the given values
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+
+# Use the count() method to find how many times 4 appears in the tuple
+count_of_4 = tuplex.count(4)
+
+# Print the result
+print("The number 4 appears", count_of_4, "times in the tuple.")
+
+
+# Output:
+
+# The number 4 appears 3 times in the tuple.
---
+++ Python_Day14_Tuples_Assigment 2.py
@@ -0,0 +1,16 @@
+# Write a Python program to convert a list to a tuple.
+# Input: listx = [5, 10, 7, 4, 15, 3] Output: (5, 10, 7, 4, 15, 3)
+
+# Define the list with the given values
+listx = [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple using the tuple() function
+tuplex = tuple(listx)
+
+# Print the result
+print("The tuple is:", tuplex)
+
+
+# Output:
+
+# The tuple is: (5, 10, 7, 4, 15, 3)
